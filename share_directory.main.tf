02817aea1bafbae3341c89c7965fc6814ec0c0c3
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-07-14 14:12:41 +0200

    Remove `storage_account_name` argument as it is deprecated and rename `share_name` to `share`.

--- share_directory.main.tf
+++ share_directory.main.tf
@@ -1,16 +1,11 @@
 resource "azurerm_storage_share_directory" "share__directory" {
   for_each = {
     for share_directory in var.share_directories :
-    "${share_directory.share_name}__${share_directory.name}" => share_directory
+    "${share_directory.share}__${share_directory.name}" => share_directory
     if share_directory.name != null
   }
 
-  name                 = each.value.name
-  share_name           = each.value.share_name
-  storage_account_name = coalesce(each.value.storage_account_name, var.name)
-  metadata             = each.value.metadata
-
-  depends_on = [
-    azurerm_storage_share.name,
-  ]
+  name             = each.value.name
+  storage_share_id = azurerm_storage_share.name[each.value.share].id
+  metadata         = each.value.metadata
 }
--- share_directory.vars.tf
+++ share_directory.vars.tf
@@ -1,10 +1,9 @@
 variable "share_directories" {
   type = list(object({
-    name                 = string                # The name (or path) of the Directory that should be created within this File Share.
-    share_name           = string                # The name of the File Share where this Directory should be created.
-    storage_account_name = optional(string)      # The name of the Storage Account within which the File Share is located.
-    metadata             = optional(map(string)) # A mapping of metadata to assign to this Directory.
+    name     = string                # The name (or path) of the Directory that should be created within this File Share.
+    share    = string                # The name or id of the File Share where this Directory should be created.
+    metadata = optional(map(string)) # A mapping of metadata to assign to this Directory.
   }))
   description = "List of directories within an Azure Storage File Share."
-  default     = [{ name = null, share_name = null }]
+  default     = [{ name = null, share = null }]
 }
